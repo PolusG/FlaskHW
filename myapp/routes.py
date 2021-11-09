19d9a7aa2722a103290a88e2e01870a97de78441
Polus Gorgees <polusgorgees123@gmail.com>
2021-11-08 20:38:00 -0800

    added myapp and run.py

---
+++ myapp/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, url_for, flash, redirect
+from myapp.forms import TopCities
+from myapp import app
+
+top_cities = [
+    "Paris", "London", "Rome", "Tahiti"
+]
+
+
+title = 'Top Cities'
+
+name = 'Polus'
+
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    form = TopCities()
+    if form.validate_on_submit():
+        flash(f'Form Submitted for {form.city_name.data}!',
+              category='success')
+        return redirect(url_for('home'))
+
+    return render_template("home.html",
+                           title=title,
+                           name=name,
+                           top_cities=top_cities,
+                           form=form)
